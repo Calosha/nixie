040f2f2c44a02859223f631769db672c4798e465
Kalosha <alexey.chernyuk@gmail.com>
2018-07-19 22:42:33 -0400

    show time on 4 lamps
    show time on 4 lamps

--- test.py
+++ test.py
@@ -6,12 +6,36 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 #pin setup
 #to do: move to settings.json
-A = 6 
+
+# Hours most significant
+A = 6
 B = 19
 C = 26
 D = 13
 
-PINGROUP = [D,C,B,A]
+# Hours least significant
+A1 = 12 
+B1 = 16
+C1 = 20
+D1 = 21
+
+# Minutes most significant
+A2 = 18
+B2 = 23
+C2 = 24
+D2 = 25
+
+# Minutes least significant
+A3 = 4
+B3 = 17
+C3 = 27
+D3 = 22
+
+HRM = [D, C, B, A]
+HRL = [D1, C1, B1, A1]
+MNM = [D2, C2, B2, A2]
+MNL = [D3, C3, B3, A3]
+
 NUMBERS = ({'1': [0, 0, 0, 0],
 			'0': [0, 0, 0, 1],
 			'9': [0, 0, 1, 0],
@@ -23,9 +47,17 @@ NUMBERS = ({'1': [0, 0, 0, 0],
 			'3': [1, 0, 0, 0],
 			'2': [1, 0, 0, 1]})
 
-def showDigit(digit, delay):
-	GPIO.output(PINGROUP, NUMBERS[digit])
+def showDigit(digit, pingroup):
+	GPIO.output(pingroup, NUMBERS[digit])
 	time.sleep(delay)
+
+def showTime(hours, minutes):
+	showDigit(hours[0], HRM)
+	showDigit(hours[1], HRL)
+	showDigit(minutes[0], MNM)
+	showDigit(minutes[1], MNL)
+
+
 
 def antiPoisoning():
 	for i in range(10):
@@ -34,11 +66,30 @@ def antiPoisoning():
 	for i in range(9, -1, -1):
 		GPIO.output(PINGROUP, NUMBERS[str(i)])
 		time.sleep(0.2)
-
+#hours most
 GPIO.setup(A, GPIO.OUT)
 GPIO.setup(B, GPIO.OUT)
 GPIO.setup(C, GPIO.OUT)
 GPIO.setup(D, GPIO.OUT)
+
+#hours least
+GPIO.setup(A1, GPIO.OUT)
+GPIO.setup(B1, GPIO.OUT)
+GPIO.setup(C1, GPIO.OUT)
+GPIO.setup(D1, GPIO.OUT)
+
+#mins most
+GPIO.setup(A2, GPIO.OUT)
+GPIO.setup(B2, GPIO.OUT)
+GPIO.setup(C2, GPIO.OUT)
+GPIO.setup(D2, GPIO.OUT)
+
+#mins least
+GPIO.setup(A3, GPIO.OUT)
+GPIO.setup(B3, GPIO.OUT)
+GPIO.setup(C3, GPIO.OUT)
+GPIO.setup(D3, GPIO.OUT)
+
 while True:
 	hr = time.strftime('%I')
 	mn = time.strftime('%M')
@@ -48,8 +99,5 @@ while True:
 	print(mn)
 	if mn == '30' or mn == '00':
 		antiPoisoning()
-
-	for h in hr:
-		showDigit(h,1)
-	for m in mn:
-		showDigit(m,1)
+    time.sleep(60)
+    showTime()
